69e2425ba869ad3cb3f0bbe7af8f9c1c1415a4a1
rgrannell1 <r.grannell2@gmail.com>
2013-12-15 20:26:56 +0000

    initial commit.

---
+++ bang.js
@@ -0,0 +1,75 @@
+
+/*
+	Bang! JS
+	A node server for redirecting queries to other search engines.
+
+*/
+
+var url = require('url')
+var http = require('http')
+
+var consts = {
+	port: 8125,
+	templates: {
+		"!w": function (searchTerm) {
+			return "https://en.wikipedia.org/wiki/" + searchTerm
+		},
+		"!g": function (searchTerm) {
+			return "https://encrypted.google.com/search?hl=en&q=" + searchTerm
+		}
+	}
+}
+
+
+
+
+var redirect = function (searchTerms) {
+	/*
+		string -> string
+		given the search terms and possibly
+		a search engine designator return a
+		url that queries that given engine.
+	*/
+
+	for (bang in consts.templates) {
+		if (!consts.templates.hasOwnProperty(bang)) {
+			continue
+		}
+
+		var bangRegExp = new RegExp(bang)
+		var isMatch = bangRegExp.test(searchTerms)
+
+		if (isMatch) {
+
+			var searchTerms = searchTerms.replace(bang, '')
+			var redirecter = consts.templates[bang]
+
+			return redirecter(searchTerms)
+		}
+	}
+
+	var redirecter = consts.templates['!g']
+
+	return redirecter(searchTerms)
+}
+
+
+
+http.createServer( function (request, response) {
+
+	var searchTerms = url.parse(request.url, true).query.q
+
+	if (searchTerms !== undefined) {
+
+		var redirectURL = redirect(searchTerms)
+
+		response.statusCode = 302
+		response.setHeader('Location', redirectURL)
+	}
+
+	response.end('')
+
+} ).
+listen(consts.port)
+
+console.log('bang! js server listening at http://127.0.0.1:8125/')
